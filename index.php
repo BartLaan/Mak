e9466f4b6c9afd2d2734cfa5f3c5f5d0db3151bd
rien333 <rien334@gmail.com>
2015-01-15 13:47:52 +0100

    Databese gefixt?

--- index.php
+++ index.php
@@ -169,7 +169,7 @@
         <?php
             $naam;    
 
-            $db = new PDO('mysql:host=http://ki30.webdb.fnwi.uva.nl/; dbname=Mak', 'rijnder', 'GodspeedF#A#');
+            $db = new PDO('localhost; dbname=Mak', 'rijnder', 'GodspeedF#A#');
             $sql = "SELECT ProductNaam FROM Test LIMIT 1";
             $result = $db->exec($sql);
             echo "Test: " . $naam;
