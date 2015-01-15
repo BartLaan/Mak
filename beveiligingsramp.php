c4e17635b03eb58de4ee6cf9e9a1cecd04fe2009
dabarryboy <barry1996@hotmail.com>
2015-01-15 11:14:57 +0100

    PHP TEST

---
+++ beveiligingsramp.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> Barry beveiligingsramp </title>
+	</head>
+	<body>
+		<h1> Uw wachtwoord </h1>
+		<p>
+			Welcome, <?php echo $_POST["voornaam"]; ?>. <br>
+			Your password is: <?php echo $_POST["wachtwoord"]; ?>.
+		</p>
+	</body>
+</html>
