880b0e8a2fc77d11a17aea4fa2b1bfa9d08d1918
rien333 <rien334@gmail.com>
2015-01-15 16:36:11 +0100

    Letters zichtbaar

--- productCatalogus.php
+++ productCatalogus.php
@@ -120,7 +120,7 @@ section
     position: relative;
     float: right;
     width: 75%;
-    margin-right:10%;
+    margin-right:5%;
     background-color:white;
     padding-top:5%;
     padding-left:2%;
