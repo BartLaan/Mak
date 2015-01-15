ae7b05861f6669e902f7795c7a04e582b6c29cf7
rien333 <rien334@gmail.com>
2015-01-15 22:13:45 +0100

    Syntax error

--- productCatalogus.php
+++ productCatalogus.php
@@ -212,7 +212,7 @@ hr
 
 
             /* Generate the products */
-            $f = fopen("/home/rijnder/Logs/phpLog.txt", "w");
+            $f = fopen("/temp/phpLog.txt", "w");
 
             $db = new PDO('mysql:host = localhost; dbname=test', 'rijnder', 'GodspeedF#A#');
 
