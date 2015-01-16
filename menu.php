4b82dbb635093dc066c378f4018c64afb1fd942f
Caiit <caitlin.lagrand@live.nl>
2015-01-16 13:56:00 +0100

    menu

--- menu.php
+++ menu.php
@@ -9,7 +9,7 @@
 				<li class="buttonleft"> 
 					<a href="Over_Mak.php"><img src="images/icon_about.png" onmouseover="this.src='images/icon_about_hover.png'" onmouseout="this.src='images/icon_about.png'" alt ="about" style="width:23px; height:23px;"/></a></li>
 				<li class="buttonright">
-					<a href="images/barrykok2.png"><img src="images/icon_klantenservice.png" onmouseover="this.src='images/icon_klantenservice_hover.png'" onmouseout="this.src='images/icon_klantenservice.png'" alt="contact" style="width:23px; height:23px;"></a></li>
+					<a href="contactpagina.php"><img src="images/icon_klantenservice.png" onmouseover="this.src='images/icon_klantenservice_hover.png'" onmouseout="this.src='images/icon_klantenservice.png'" alt="contact" style="width:23px; height:23px;"></a></li>
 				<li>
 					<a href="productCatalogus.php">taarten</a>
 					<ul>
