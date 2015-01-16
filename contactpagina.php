bcc789625e47f31eabc75d601606bca6f56bb6ab
BartLaan <bartlaan96@gmail.com>
2015-01-16 13:32:08 +0100

    Contacten naar contactpagina

---
+++ contactpagina.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>
+			Contact
+		</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
+    <link href="opmaakmenu.css" rel="stylesheet" type="text/css" />
+
+    <style>
+    h1.about, p.about
+    {
+        color: black;
+    }    
+    .paginaSectie
+    {
+        padding:5%;
+        padding-right:-3%;
+        margin-top: 5%;
+        background-color: #e3e7ea;
+        border-radius:10px;
+        border: solid #d6dbe0;
+    }
+    table td
+    {
+        padding-right:20px;   
+    }
+
+    </style>
+
+	</head>
+
+
+	<body>
+		<?php include 'menu.php'; ?>
+		<div class="paginaSectie">
+
+                <h1> Contact </h1>
+                <p> Voor vragen of opmerking kunt u contact met ons opnemen via mail of langskomen op onze locatie. </p>
+
+        		<p> 
+        			<h3> Adres Gegevens </h3>
+                    <address>
+        			Mak <br>
+        			<i> <b>Science Park 904, </b></i><br>
+                    1098 XH Amsterdam   <br>
+                    </address>
+        		</p>
+
+                <h3> Emailadressen </h2>
+        		<table>
+        			<tr>
+        				<td>Martijn Hoogwerf: </td>
+        				<td>
+                        <a href="mailto:martijn_hoogwerf@hotmail.com">martijn_hoogwerf@hotmail.com</a>
+                        </td>
+        			</tr>
+        			<tr>
+        				<td>Bart Laan: </td>
+        				<td>
+                        <a href="mailto:bart.laan@gmail.com ">bart.laan@gmail.com </a>
+                        </td>
+        			</tr>
+        			<tr>
+        				<td>Caitlin Lagrand: </td>
+        				<td>
+                        <a href="mailto:caitlin.lagrand@live.nl">caitlin.lagrand@live.nl</a> 
+                        </td>
+        			</tr>
+        			<tr>
+        				<td>Barry Servaas: </td>
+        				<td>
+                        <a href="mailto:barry1996@hotmail.com ">barry1996@hotmail.com </a> 
+                        </td>
+        			</tr>
+        			<tr> 
+        				<td>Simon Verboom: </td>
+        				<td>
+                        <a href="mailto:sverboom1994@gmail.com">sverboom1994@gmail.com </a>
+                        </td>
+        			</tr>
+        			<tr>
+        				<td>Rijnder Wever: </td>
+        				<td>
+                        <a href="mailto:rien334@gmail.com">rien334@gmail.com </a>
+                         </td>
+        			</tr>
+        		</table>            
+            </div>
+	</body>
+
+<html>
