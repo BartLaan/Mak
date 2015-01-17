927668af8aec26b6bd3908a6473ded6d7ea8b5cb
rien333 <rien334@gmail.com>
2015-01-17 02:01:49 +0100

    Slideshow

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         min-width:320px;
         overflow:hidden;
         position: absolute;
-        top:-50%;
+        top:-25%;
         border-color:red;
         min-height:300px;
         height:485px;
