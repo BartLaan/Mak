a268c23776b062a7970c0ce9e09a09fbf61af44c
dabarryboy <barry1996@hotmail.com>
2015-01-19 16:15:37 +0100

    Gebruiker registreren test 1

--- gebruiker_registreren.php
+++ gebruiker_registreren.php
@@ -203,7 +203,7 @@
 					try{
 						$db = new PDO('mysql:host = localhost; db = test', 'barry', 'Mz89WTxa');
 						$sql = "INSERT INTO Gebruikers (Voornaam, Tussenvoegsel, Achternaam, Geslacht, Straat, Huisnummer, Postcode, Woonplaats, Telefoonnummer, Emailadres, Wachtwoord);
-								VALUES ($FIRSTNAME, $TUSSENVOEGSEL, $LASTNAME, $GENDERHELPER, $STREET, $HOUSE, $ZIP, $DOM, $PHONE, $MAIL, $PASS);"} 
+								VALUES ($FIRSTNAME, $TUSSENVOEGSEL, $LASTNAME, $GENDERHELPER, $STREET, $HOUSE, $ZIP, $DOM, $PHONE, $MAIL, $PASS)";} 
 								catch(PDOException $ex){
 						die("Het is op dit moment niet mogelijk om met de database verbinding te maken. Probeer het alstublieft later nog een keer.");
 					}
