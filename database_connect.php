a51e7d54ffd2c54ea43d8d9208a5f6835a9aa950
BartLaan <bartlaan96@gmail.com>
2015-01-19 12:47:26 +0100

    minor change db_connect

--- database_connect.php
+++ database_connect.php
@@ -1,7 +1,7 @@
 <?php
 	$dbconf = simplexml_load_file("/var/secure/mysql_config_mak.xml");
 	if ($dbconf == FALSE) {
-		die("Error parsing XML file")
+		die("Error parsing XML file");
 	} else {
 		$db = new PDO("mysql:host=$dbconf->mysql_host;dbname=$dbconf->mysql_database;charset=utf8",
 		"$dbconf->mysql_username", "$dbconf->mysql_password")
