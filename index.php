10dc4c5ef4b75082190b1b4dcf320451773c05d0
rien333 <rien334@gmail.com>
2015-01-19 23:43:52 +0100

    Algemene interface vorm geimplementeert

--- index.php
+++ index.php
@@ -235,7 +235,7 @@
             </div>
         </div>
 
-            <div class="afbeeldingKop" style="opacity: 0.8; margin-top:-48.5%; ">
+            <div class="afbeeldingKop" style="opacity: 0.8; margin-top:-48%; ">
             <div class="achtergrondVak" style="background-image: url(images/Taart2.jpg);">
 
             </div>
