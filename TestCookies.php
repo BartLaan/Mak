b6f96ad2b5a3c8152b503a0308629f32ab8655e1
BartLaan <bartlaan96@gmail.com>
2015-01-19 11:52:03 +0100

    testcookies

--- TestCookies.php
+++ TestCookies.php
@@ -10,6 +10,9 @@ session_start();
 E-mail: <input type="text" name="email"><br>
 Wachtwoord: <input type="password" name="wachtwoord"><br>
 <input type="submit" value="log in">
+<?php
+	$_SESSION['email'] = $_POST['email'];
+?>
 </form>
 </body>
 </html>
