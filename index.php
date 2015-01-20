d9d84799b2a752a8343405679aa5df489885ab1c
rien333 <rien334@gmail.com>
2015-01-20 15:14:12 +0100

    Algemene interface vorm geimplementeert

--- index.php
+++ index.php
@@ -412,7 +412,6 @@
     var timer = setInterval(function () {
         if (op <= 0.1){
             clearInterval(timer);
-            element.style.display = 'none';
         }
         element.style.opacity = op;
         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
