2ff028380730e4f13922ecdf6abce2b7aa0dc213
rien333 <rien334@gmail.com>
2015-01-20 01:13:58 +0100

    Algemene interface vorm geimplementeert

--- index.php
+++ index.php
@@ -274,16 +274,9 @@
         var listElement = document.getElementById("afbeeldingKop3");
 
         var title = listElement.getElementsByTagName("div");
-        var imageOpacity (window.getComputedStyle(title[0]).opacity;
-        alert(imageOpacity);
-        alert("Ye");
-        var imageOpacity = 
-        window.getComputedStyle(document.getElementById("afbeeldingKop3")).opacity;
-        document.write("test");
-        alert(imageOpacity);
-        document.getElementById("afbeeldingKop3").style.opacity = imageOpacity - 0.7; 
+        var imageOpacity = window.getComputedStyle(title[0]).opacity;
+        alert(imageOpacity);        document.getElementById("afbeeldingKop3").style.opacity = imageOpacity - 0.7; 
 
-        slideShow(getAfbeeldingKoppen());
 
 
         function getAfbeeldingKoppen()
