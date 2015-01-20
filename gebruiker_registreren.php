688cbb6fb276708cf1a4c53666e9d6139bbe468b
Caiit <caitlin.lagrand@live.nl>
2015-01-20 13:52:35 +0100

    opmaak

--- gebruiker_registreren.php
+++ gebruiker_registreren.php
@@ -202,7 +202,7 @@
 				if($CORRECTNESS == TRUE){
 					try{
 						$db = new PDO('mysql:host = localhost; db = test', 'barry', 'Mz89WTxa');
-						$sql = $db -> prepare('SELECT * FROM Gebruikers');
+						$sql = $db -> prepare('SELECT * FROM Gebruikers WHERE Klant_ID = 1');
 						$sql -> execute();
 						$result = $sql -> fetchAll(PDO::FETCH_ASSOC);
 						foreach($results as $row){
