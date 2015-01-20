aa025f546b7a78badafdeb6df5e2e2a4979c3ddb
Caiit <caitlin.lagrand@live.nl>
2015-01-20 13:08:53 +0100

    opmaak

--- ProductPagina.php
+++ ProductPagina.php
@@ -190,7 +190,7 @@ h4.tekstKop
                     echo "</div> ";
         
         
-                    echo "<form method='POST' action="htmlspecialchars($_SERVER['PHP_SELF'])" >"; 
+                    echo "<form >"; 
                         echo "<h4 class='tekstKop'>Naam</h4>";
                         echo "<input type='text' name='naam'> ";
                         echo "<h4 class='tekstKop'>Recensie</h4>";
