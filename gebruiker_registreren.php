41267757d94accdef57b0841dc21c4702d748e7c
Caiit <caitlin.lagrand@live.nl>
2015-01-20 13:42:49 +0100

    opmaak

--- gebruiker_registreren.php
+++ gebruiker_registreren.php
@@ -206,7 +206,8 @@
 						$sql -> execute();
 						$result = $sql -> fetchAll(PDO::FETCH_ASSOC);
 						foreach($results as $row){
-							echo $row['Voornaam'];
+							echo "<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+							echo "$row['Voornaam'];
 						}
 					//	$sql = $db->prepare('INSERT INTO Gebruikers(Voornaam, Tussenvoegsel, Achternaam, Geslacht, Straat, Huisnummer, Postcode, Woonplaats, Telefoonnummer, Emailadres, Wachtwoord)
 					//						VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
