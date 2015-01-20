1db874bc101c3f85b0291f0fd7bccc2b22a36371
BartLaan <bartlaan96@gmail.com>
2015-01-20 13:21:04 +0100

    heredoc proberen

--- TestCookies.php
+++ TestCookies.php
@@ -5,21 +5,33 @@
 <body>
 <?php
 	session_start();
-?>
-<form action="index.php" method="post">
+
+	if(!empty ($_POST['username'])) {
+		$sha1ww = sha1($_POST['wachtwoord']);
+		include "database_connect.php";
+		$sqlww = 'SELECT Wachtwoord FROM Klanten WHERE Emailadres =' . $_POST['email'];
+		$success = false;
+		if ($sqlww[0] !== $sha1ww) {
+			echo 'Je moeder';
+		} else {
+			$_SESSION['email'] = $_POST['email'];
+		}
+	} else {
+		echo <<<E0T
+<!DOCTYPE html>
+<html>
+<head>
+<title>Inloggen</title>
+</head>
+<body>
+<form action="{$_SERVER['PHP_SELF']}" method="post">
 E-mail: <input type="text" name="email"><br>
 Wachtwoord: <input type="password" name="wachtwoord"><br>
 <input type="submit" value="log in">
 </form>
-<?php
-	$sha1ww = sha1($_POST['wachtwoord']);
-	include "database_connect.php";
-	$sqlww = 'SELECT Wachtwoord FROM Klanten WHERE Emailadres =' . $_POST['email'];
-	if ($sqlww !== $sha1ww) {
-		echo 'Je moeder';
-	} else {
-		$_SESSION['email'] = $_POST['email'];
-	}
+</body>
+</html>
+E0T;
 ?>
 </body>
 </html>
