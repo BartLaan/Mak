3f7cce71ef747a69ae2979fd5c451617f24710c9
Caiit <caitlin.lagrand@live.nl>
2015-01-21 21:52:55 +0100

    winkelwagen

--- winkelwagen.js
+++ winkelwagen.js
@@ -1,4 +1,4 @@
 function inWinkelwagen() {
                         $_SESSION['winkelwagen'] = "$row["Product_ID"]";
-                        document.getElementByID("test").innerHTML = "$row["Product_ID"]";
+                        document.getElementByID("test").innerHTML = "test";
                     }
