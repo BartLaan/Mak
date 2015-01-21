352e4739027729fdac02144d0b2b772ca392bdd4
BartLaan <bartlaan96@gmail.com>
2015-01-21 16:46:23 +0100

    mysql config weer verwijderd en log_in update

--- log_in.php
+++ log_in.php
@@ -9,42 +9,43 @@
 <?php include 'menu.php'; ?>
 	<div id="page">
     <div id="text">
-<?php
+    <br />
+	<?php
 
-	if (@$_SERVER['HTTPS'] !== 'on') {
-		$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-		header("Location: $redirect", true, 301);
-		exit();
-	}
-	session_start();
+		if (@$_SERVER['HTTPS'] !== 'on') {
+			$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+			header("Location: $redirect", true, 301);
+			exit();
+		}
+		session_start();
 
-	// Inlogdata valideren
-	if(!empty ($_POST['email']) && !empty ($_POST['wachtwoord'])) {
-		$sha1ww = sha1($_POST['wachtwoord']);
-		include "database_connect.php";
+		// Inlogdata valideren
+		if(!empty ($_POST['email']) && !empty ($_POST['wachtwoord'])) {
+			$sha1ww = sha1($_POST['wachtwoord']);
+			include "database_connect.php";
 
-		$_SESSION['login_success'] = false;
+			$_SESSION['login_success'] = false;
 
-		$query = "SELECT * FROM Klant WHERE Emailadres ='" . $_POST['email'] . "'AND Wachtwoord='" . $sha1ww . "'";
-        $stmt = $db->prepare($query);
-        $stmt->execute();
+			$query = "SELECT * FROM Klant WHERE Emailadres ='" . $_POST['email'] . "'AND Wachtwoord='" . $sha1ww . "'";
+	        $stmt = $db->prepare($query);
+	        $stmt->execute();
 
-		$result = $stmt->fetch(); 
-		if ($result && strlen($result["Emailadres"]) > "0") {
-			echo "Inloggen gelukt";
-       		$_SESSION['login_success'] = true;
-			$_SESSION['email'] = $_POST['email'];
+			$result = $stmt->fetch(); 
+			if ($result && strlen($result["Emailadres"]) > "0") {
+				echo "Inloggen gelukt";
+	       		$_SESSION['login_success'] = true;
+				$_SESSION['email'] = $_POST['email'];
+			} else {
+				header('Location: ' . $_SERVER['PHP_SELF']);
+			}
 		} else {
-			header('Location: ' . $_SERVER['PHP_SELF']);
-		}
-	} else {
-		if (isset($_SESSION['login_success']) && !$_SESSION['login_success']) {
-			echo "Inloggen niet gelukt: "; ?> <br /> <?php
-			echo "Foute E-mailadres/wachtwoord combinatie.";
-			unset($_SESSION['login_success']);
-		}
-	// Formulier:
-		echo <<<EOT
+			if (isset($_SESSION['login_success']) && !$_SESSION['login_success']) {
+				echo "Inloggen niet gelukt: "; ?> <br /> <?php
+				echo "Foute E-mailadres/wachtwoord combinatie.";
+				unset($_SESSION['login_success']);
+			}
+		// Inloginvoerdingen
+			echo <<<EOT
 <!DOCTYPE html>
 <html>
 <head>
