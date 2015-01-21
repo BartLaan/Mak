eba7a3d3dc7879537bdaf554dcdf07a1edd98c4a
rien333 <rien334@gmail.com>
2015-01-21 02:30:41 +0100

    Sidebar fix

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-    <div id="footer" style="z-index:90;background-color: #fff4e6;">
+    <div id="footer" style="z-index:90;background-color: red;">
     <ul>
         <li class="logo">
                 <img src="images/barrylogo2.png" alt="logo" style="width:192.4px;height:43.8px;"></li>
