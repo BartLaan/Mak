4c9b7d8eaa76f409e187473d736f6192222e1081
dabarryboy <barry1996@hotmail.com>
2015-01-21 16:17:18 +0100

    registratietest 19

--- gebruiker_registreren.php
+++ gebruiker_registreren.php
@@ -113,12 +113,15 @@
 						$CORRECTNESS = FALSE;
 					}
 					else if($GENDER == "overig"){
+						die($GENDER);
 						$GENDERHELPER = 0;
 					}
 					else if($GENDER == "man"){
+						die($GENDER);
 						$GENDERHELPER = 1;
 					}
 					else if($GENDER == "vrouw"){
+						die($GENDER);
 						$GENDERHELPER = 2;
 					}
 				}
