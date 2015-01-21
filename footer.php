e6e3fb61444b7cc240fe4ed0d90a7bb71dcc22b9
rien333 <rien334@gmail.com>
2015-01-21 01:52:45 +0100

    Slideshow

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-    <div id="footer" style="z-index:90; width:25%;height:3%; max-height:7%;">
+    <div id="footer" style="z-index:90; width:3%;height:3%; max-height:7%;">
     <ul>
         <li class="logo">
                 <img src="images/barrylogo2.png" alt="logo" ></li>
