08287066db41c33f049cfbb0a1f652b5c85db532
rien333 <rien334@gmail.com>
2015-01-22 00:31:44 +0100

    Sortering

--- printProducten.php
+++ printProducten.php
@@ -1,93 +1,90 @@
 
 
 <?php 
-    function printProducten()
-            {
-                $f = fopen("/tmp/phpLog.txt", "w");
 
-                $db = new PDO('mysql:host = localhost; dbname=Mak', 'rijnder', 'GodspeedF#A#');
-                $db->setAttribute(PDO::ERRMODE_SILENT,PDO::CASE_NATURAL);
-                
-                $orderingColumn = "Productnaam";
+    $f = fopen("/tmp/phpLog.txt", "w");
 
-
-                $disabledCategories = array();
-                foreach($_GET as $key => $value)
-                {
-                    if(substr( $key, 0, 3) === "cat")
-                    {
-                       array_push($disabledCategories, $value);
-                    }
-                    if(substr( $key, 0, 3) === "ord")
-                    {
-                        $orderingColumn = $value;
-                    }
-                }
-
-                fwrite($f , print_r($disabledCategories,true) . "\n");
-
-                $productenSql = "SELECT TRIM(LEADING '0'
-    FROM Prijs), Productnaam, SecundaireInfo, img_filepath, Aanbieding, Product_ID
-    FROM Product ORDER BY " . $orderingColumn;
-                if(count($disabledCategories) > 0)
-                {
-                    $productenSql .= " WHERE  (";
-                    foreach($disabledCategories as $disabledCategorie)
-                    {
-                        $productenSql .= ' Categorie != "' . $disabledCategorie . '" OR ';
-                    }
-                    $productenSql = substr($productenSql, 0, -4);
-                    $productenSql .= ")";
-                }
-                
-                $stmt = $db->prepare($productenSql); 
-                $stmt->execute();
-     
-                while($row =$stmt->fetch() )
-                {
-                    // Not sure if '#' is correct here
-                    $id = $row["Product_ID"];
-                    echo "<a class ='product' href='ProductPagina.php?id=$id'>" ;
-                    echo '<div class="productAfbeelding">';
-                    echo '<img src="images/' . $row["img_filepath"] . '" alt="' . $row["Productnaam"] . '"></img><br>';
-                    echo '</div>';
-                    echo ' <hr>';
-                    // Geen ondersteuning speciale chars
+    $db = new PDO('mysql:host = localhost; dbname=Mak', 'rijnder', 'GodspeedF#A#');
+    $db->setAttribute(PDO::ERRMODE_SILENT,PDO::CASE_NATURAL);
     
-                    // Niet de juiste manier
+    $orderingColumn = "Productnaam";
+
+
+    $disabledCategories = array();
+    foreach($_GET as $key => $value)
+    {
+        if(substr( $key, 0, 3) === "cat")
+        {
+            array_push($disabledCategories, $value);
+        }
+        if(substr( $key, 0, 3) === "ord")
+        {
+            $orderingColumn = $value;
+        }
+    }
+
+    fwrite($f , print_r($disabledCategories,true) . "\n");
+
+    $productenSql = "SELECT TRIM(LEADING '0'
+FROM Prijs), Productnaam, SecundaireInfo, img_filepath, Aanbieding, Product_ID
+FROM Product ORDER BY " . $orderingColumn;
+    if(count($disabledCategories) > 0)
+    {
+        $productenSql .= " WHERE  (";
+        foreach($disabledCategories as $disabledCategorie)
+        {
+            $productenSql .= ' Categorie != "' . $disabledCategorie . '" OR ';
+        }
+        $productenSql = substr($productenSql, 0, -4);
+        $productenSql .= ")";
+    }
     
-                    echo '<div class="productNaam">' .  $row['Productnaam']. '</div>';
-    
-                    if ( strlen($row["SecundaireInfo"]) != NULL)
-                    {
-                        echo '<span class="secundaire-info">' . $row["SecundaireInfo"] . '</span>';
-                    }
+    $stmt = $db->prepare($productenSql); 
+    $stmt->execute();
+
+    while($row =$stmt->fetch() )
+    {
+        // Not sure if '#' is correct here
+        $id = $row["Product_ID"];
+        echo "<a class ='product' href='ProductPagina.php?id=$id'>" ;
+        echo '<div class="productAfbeelding">';
+        echo '<img src="images/' . $row["img_filepath"] . '" alt="' . $row["Productnaam"] . '"></img><br>';
+        echo '</div>';
+        echo ' <hr>';
+        // Geen ondersteuning speciale chars
+
+        // Niet de juiste manier
+
+        echo '<div class="productNaam">' .  $row['Productnaam']. '</div>';
+
+        if ( strlen($row["SecundaireInfo"]) != NULL)
+        {
+            echo '<span class="secundaire-info">' . $row["SecundaireInfo"] . '</span>';
+        }
+
+        echo "<br>";
+
+        if ( strlen($row["Productnaam"]) < 22 )
+        {
+            echo "<br>";
+        }
         
-                    echo "<br>";
-    
-                    if ( strlen($row["Productnaam"]) < 22 )
-                    {
-                        echo "<br>";
-                    }
-                    
-                    if( $row['Aanbieding'] == 0)    // Geen aanbieding
-                    {   
-                        echo "<br>";
-                        echo '<span class="prijstekst"> &euro;' . $row["TRIM(LEADING '0'
-    FROM Prijs)"] . '</span>';
-                    }
-                    else
-                    {
-                        echo '<span class="prijstekst" id="afgeprijst"> &euro;' .$row["TRIM(LEADING '0'
-    FROM Prijs)"] . '</span>';
-                        echo '<br><span class="afgeprijst">&euro;' . $row["Aanbieding"] . ' </span>';
-                    }
-    
-                    echo "<br>";
-                    echo "</a>";    
-                }
-                fclose($f); 
+        if( $row['Aanbieding'] == 0)    // Geen aanbieding
+        {   
+            echo "<br>";
+            echo '<span class="prijstekst"> &euro;' . $row["TRIM(LEADING '0'
+FROM Prijs)"] . '</span>';
+        }
+        else
+        {
+            echo '<span class="prijstekst" id="afgeprijst"> &euro;' .$row["TRIM(LEADING '0'
+FROM Prijs)"] . '</span>';
+            echo '<br><span class="afgeprijst">&euro;' . $row["Aanbieding"] . ' </span>';
+        }
 
-            }
-    printProducten();
+        echo "<br>";
+        echo "</a>";    
+    }
+    fclose($f); 
+
 ?>
