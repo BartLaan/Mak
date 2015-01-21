ef0bead366da8814d69d85f3868f22086c2a86b1
rien333 <rien334@gmail.com>
2015-01-21 01:23:15 +0100

    Slideshow

--- index.php
+++ index.php
@@ -463,7 +463,7 @@
             }
             image1.style.opacity = op1;
             image1.style.filter = 'alpha(opacity=' + op1 * 100 + ")";
-            op1 -= 0.1;
+            op1 -= 0.08;
 
             // Fade in
             if (op2 >= 1)
@@ -475,10 +475,10 @@
             }
             image2.style.opacity = op2;
             image2.style.filter = 'alpha(opacity=' + op2 * 100 + ")";
-            op2 += 0.1; 
+            op2 += 0.08; 
 
 
-        }, 65);
+        }, 45);
 
     }
 
