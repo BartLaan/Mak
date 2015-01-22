2188bc7d0626cab243bc482259859471e2993937
Caiit <caitlin.lagrand@live.nl>
2015-01-22 15:58:34 +0100

    menu

--- menu.php
+++ menu.php
@@ -36,7 +36,7 @@
             			$stmt = $db->prepare($categorieSql); 
             			$stmt->execute();
             			while($row =$stmt->fetch() ) {
-            				echo '<li>'.$row["Categorie"].'</li> ';
+            				echo '<li><a href="productCatalogus.php">'.$row["Categorie"].'</a></li> ';
         				}
         				?>
 					</ul> 
