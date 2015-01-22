c4df1e0df7a33905d89e1aaa2b5d2c3978244cb2
Caiit <caitlin.lagrand@live.nl>
2015-01-22 15:55:01 +0100

    menu

--- menu.php
+++ menu.php
@@ -22,13 +22,13 @@
             			$stmt = $db->prepare($categorieSql); 
             			$stmt->execute();
             			while($row =$stmt->fetch() ) {
-            				echo '<li>$row["Categorie"]</li> ';
+            				echo '<li>'.$row["Categorie"].'</li> ';
         				}
 						/*<li><a href="productCatalogus.php">taarten</a></li>
 						<li><a href="productCatalogus.php">koekjes</a></li>
 						<li><a href="productCatalogus.php">cupcakes</a></li>
 						<li><a href="productCatalogus.php">cakes</a></li>*/
-						
+
         				?>
 					</ul> 
 				</li> 
