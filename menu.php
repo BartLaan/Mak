022a21c901c9748cbc5150f725b5beba9ad6b10b
Caiit <caitlin.lagrand@live.nl>
2015-01-22 16:00:56 +0100

    menu

--- menu.php
+++ menu.php
@@ -16,6 +16,7 @@
 				<li class="categorie">
 					<a href="productCatalogus.php"><img src="images/icon_list.png" onmouseover="this.src='images/icon_list_hover.png'" onmouseout="this.src='images/icon_list.png'" alt ="menu" style="width:23px; height:23px;"/></a>
 					<ul>
+						<li><a href="productCatalogus.php">Alle producten</a></li> 
 						<?php 
 						include 'database_connect.php';
 						$categorieSql = "SELECT DISTINCT Categorie FROM Product" ;
@@ -30,6 +31,7 @@
 				<li class="submenu">
 					<a href="productCatalogus.php">assortiment</a>
 					<ul>
+						<li><a href="productCatalogus.php">Alle producten</a></li> 
 						<?php 
 						include 'database_connect.php';
 						$categorieSql = "SELECT DISTINCT Categorie FROM Product" ;
