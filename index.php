dc5f507c5d66dbf1b6566f355d482d551793054f
rien333 <rien334@gmail.com>
2015-01-22 02:05:42 +0100

    Sortering

--- index.php
+++ index.php
@@ -461,7 +461,7 @@
             }
             image1.style.opacity = op1;
             image1.style.filter = 'alpha(opacity=' + op1 * 100 + ")";
-            op1 -= 0.08;
+            op1 -= 0.1;
 
             // Fade in
             if (op2 >= 1)
@@ -473,7 +473,7 @@
             }
             image2.style.opacity = op2;
             image2.style.filter = 'alpha(opacity=' + op2 * 100 + ")";
-            op2 += 0.08; 
+            op2 += 0.1; 
 
 
         }, 120);
