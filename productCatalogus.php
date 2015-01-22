1fe966337df2d62fff16aa37a5a85055548f6dfa
rien333 <rien334@gmail.com>
2015-01-22 02:31:05 +0100

    Sortering

--- productCatalogus.php
+++ productCatalogus.php
@@ -217,7 +217,7 @@ hr
 
     foreach($categorien as $row)
     {
-        echo '<input type="checkbox" onchange="generateCategories(this)" name="' . $row['Categorie'] . '" value="' . $row["Categorie"] . '" id ="' . $row["Categorie"]. '" checked> <a href="#' . $row["Categorie"]. '" >' . $row["Categorie"]. '</a>';      
+        echo '<input type="checkbox" onchange="generateCategories(this)" name="' . $row['Categorie'] . '" value="' . $row["Categorie"] . '" id ="' . $row["Categorie"]. '" checked> <a onclick="setCategorieSorting()" href="#' . $row["Categorie"]. '" >' . $row["Categorie"]. '</a>';      
         echo "<br>";
         $categorienArray[] = $row["Categorie"];
     }
@@ -280,6 +280,13 @@ hr
             xmlhttp.open("GET",url,true);
             xmlhttp.send();
         }
+
+        function setCategorieSorting()
+        {
+            document.getElementById("Sorting").value = "Categorie";
+            generateCategories(document.getElementById("Sorting"));
+        }
+
     </script>
 
 
