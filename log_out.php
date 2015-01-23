777fe6daeb44946d1cea5ebe1325fc8753c3dba9
BartLaan <bartlaan96@gmail.com>
2015-01-23 12:25:13 +0100

    Login comments voor salt en logout begin

---
+++ log_out.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Barry's Bakery - inloggen</title>
+	<link href="opmaakmenu.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<?php include 'menu.php';?>
+	<div id="page">
+    <div id="text">
+    <br />
+	<?php
+		if (!isset($_SESSION[''])) {
+			# code...
+		}
+?>
+</body>
+</html>
