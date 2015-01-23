b4c83b5837a644ad5b33a244308109bcf03e8e34
Caiit <caitlin.lagrand@live.nl>
2015-01-23 18:41:42 +0100

    winkelwagen

--- Winkelwagen.php
+++ Winkelwagen.php
@@ -51,8 +51,8 @@
 
     input#delete_button {
         background: url('images/prullenbak.png') no-repeat top left;
-        padding-left: 16px;
-        height: 16px;
+        height: 20px;
+        width: 20px;
     }
 
     </style>
@@ -122,7 +122,7 @@
                                         <td><a href="Winkelwagen.php"> <img src="images/prullenbak.png" alt="Verwijder artikel" style=" margin-left: 45%; margin-right: 45%; width:20px; height:20px;"> </img> </a> 
                                             <form action="Winkelwagen.php" method="post">
                                             <input type="hidden" value="'.$row['Product_ID'].'" name="delete">
-                                            <input type="submit" id="delete_button" value="Verwijder uit winkelwagen" /></form>
+                                            <input type="submit" id="delete_button" /></form>
                                         </td>
                                     </tr>';
                             }
