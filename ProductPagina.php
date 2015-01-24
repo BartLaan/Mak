93cfa35972a8037e73fe1de82a973307cd82b441
Caiit <caitlin.lagrand@live.nl>
2015-01-24 23:04:05 +0100

    recensie

--- ProductPagina.php
+++ ProductPagina.php
@@ -186,7 +186,7 @@ if (!empty($_POST['button'])) {
                 if (isset($_SESSION['login_success']) && $_SESSION['login_success'] == true) {
                     $get_klant_ID = 'SELECT Klant_ID FROM Klant WHERE Emailadres=?';
                     $statmt = $db->prepare($get_klant_ID);
-                    $statmt->bindValue(11, $email, PDO::PARAM_STR); 
+                    #$statmt->bindValue(11, $email, PDO::PARAM_STR); 
                     $statmt->execute(array($email));
                     $result = $statmt->fetchAll(PDO::FETCH_ASSOC);
 
