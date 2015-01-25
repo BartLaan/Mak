34172e8140a83127e4159f17073d0946b445639a
rien333 <rien334@gmail.com>
2015-01-25 20:47:34 +0100

    Gebruiker data aanpasbaar maken

--- ValidateKlantInput.php
+++ ValidateKlantInput.php
@@ -20,7 +20,7 @@
     $stmt->execute();
 
 
-    echo print_r(array_keys($stmt->fetch()), true);
+    echo print_r(array_keys($stmt->fetchAll()), true);
 
     echo print_r(array_keys($stmt->fetch()), true);
     echo print_r(array_keys($stmt->fetch()), true);
