3a3d752b03b0f3eaa1644404a5c6fd7e6ec61fe0
rien333 <rien334@gmail.com>
2015-01-25 20:25:32 +0100

    Gebruiker data aanpasbaar maken

--- ValidateKlantInput.php
+++ ValidateKlantInput.php
@@ -23,7 +23,7 @@
     $keysToValidate = array_intersect(array_keys($stmt->fetchAll()),  $keyArray);
 
     fwrite($f, "nice[2][2]! \n");
-    echo print_r($array_keys($stmt->fetchAll()) . "\n", true);
+    echo print_r(array_keys($stmt->fetchAll()) . "\n", true);
     echo print_r($keyArray . "\n", true);
 
     $inputCorrect = false;
