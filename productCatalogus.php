2130b4c279a55067c9e3327682c5f9b081647fbc
rien333 <rien334@gmail.com>
2015-01-26 11:36:52 +0100

    Gebruiker data aanpasbaar maken

--- productCatalogus.php
+++ productCatalogus.php
@@ -119,7 +119,7 @@ section
     background-color:white;
     padding-top:1%;
     padding-left:3%;
-    padding-right:-3%;
+    padding-right:3%;
     z-index:10;
     left:0px;
     padding-bottom:1%;
