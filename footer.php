23eae2655dd5057b163a206c09f53aa6382d6263
rien333 <rien334@gmail.com>
2015-01-26 14:19:14 +0100

    Uitstekende rand bovenaan fix

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <div id="footer">
     <ul>
         <li class="logo">
-                <img src="images/barrylogo2.png" alt="logo" style="width:195px height:auto;"></li>
+                <img src="images/barrylogo2.png" alt="logo" style="width:195px height:50px;"></li>
     </ul> 
     </div>
 
