2358c2b70ee7a3c9d0de4f9f8b4aa117d781af11
simon <simon_boompie@hotmail.com>
2015-01-26 15:40:45 +0100

    betaald.php

--- betaald.php
+++ betaald.php
@@ -41,10 +41,10 @@
         <div id="page">
         <div id="text">
         <div class="paginaSectie">
-            <p> U hebt betaald! Bedankt voor uw bestelling!
-             <a href="https://ki30.webdb.fnwi.uva.nl/Mak/productCatalogus.php">
-                <img src="images/verderwinkelen.png" border="0" alt="Winkel verder" style="width:200px">
-            </a> 
+            <p> U hebt betaald! Bedankt voor uw bestelling! <br>
+                 <a href="https://ki30.webdb.fnwi.uva.nl/Mak/productCatalogus.php">
+                    <img src="images/verderwinkelen.png" border="0" alt="Winkel verder" style="width:200px">
+                <br></a> 
             </p>          
             <img src="images/barry_banner.jpg" alt="Barry's Bakery Banner" style="width:1000px">
         </div>
