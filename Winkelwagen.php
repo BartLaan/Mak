382051934a6cac103799d2412e3a19610eee17ce
Caiit <caitlin.lagrand@live.nl>
2015-01-26 15:38:14 +0100

    bestellen

--- Winkelwagen.php
+++ Winkelwagen.php
@@ -202,9 +202,9 @@
 
                                     <form action="Winkelwagen.php" method="POST">
                                     <select name="verzending">
-                                        <option value="verzenden"' if ($verzending == 6.95) {echo 'selected';}' >
+                                        <option value="verzenden"' if ($verzending == 6.95) {echo 'selected = "selected"';}' >
                                             Verzending met PostNL (&#8364 6,95)</option>
-                                        <option value="ophalen"' if ($verzending== 0.00) {echo 'selected';}' >Ophalen (&#8364 0,00)</option>
+                                        <option value="ophalen"' if ($verzending== 0.00) {echo 'selected = "selected"';}' >Ophalen (&#8364 0,00)</option>
                                     </select>
                                     </br>
                                         <input type="submit" value="Kies">
