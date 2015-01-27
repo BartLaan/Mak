b0c2af82e76f67c233c93fc250a5f0eb2b6b82d4
rien333 <rien334@gmail.com>
2015-01-27 21:26:01 +0100

    Homepagina slideshow

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
         filter: blur(85px);
         -moz-filter: blur(85px);
         -o-filter: blur(85px);
-        -ms-filter: blur(95px); // higher value intentional
+        -ms-filter: blur(95px); 
         overflow:hidden;
         vertical-align: bottom;
         border-color:green;
