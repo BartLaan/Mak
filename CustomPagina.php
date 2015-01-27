a8af633251e4db6dc88e2b34a6719fb6441a5ec5
dabarryboy <barry1996@hotmail.com>
2015-01-27 16:08:11 +0100

    Added Cyan

--- CustomPagina.php
+++ CustomPagina.php
@@ -170,7 +170,7 @@ if (!empty($_POST['button'])) {
 <div id="page">
     <div id="text">
 		<div class ='ingredients'>
-			<div style = 'float:left; text-align:left; width:40%;'>
+			<div style = 'float:left; text-align:left; width:80%;'>
 				<h1 style ='text-align:center;'> Maak uw Eigen Taart! </h1>
 				<img src= 'images/cyan.jpg' alt ="Barry's taart" style = "width:40%; height:250px;">
 			</div>
