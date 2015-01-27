c653a2463ec3764a2bfe35581a2f84f06f78d96f
rien333 <rien334@gmail.com>
2015-01-27 12:46:04 +0100

    ProductCatalogus sorteer fix

--- productCatalogus.php
+++ productCatalogus.php
@@ -266,53 +266,53 @@ hr
 //                }
 //            }
 //
-
-
-            if(!categoriesSelected)
-            {
-                console.log("test");
-                hideFooter();
-            }
-            else
-            {
-                console.log("meh");
-            }
-
-
-            if(url.slice(-1) == "&")
-            {
-                url = url.concat("& " + "ord= " + document.getElementById("Sorting").value);
-            }
-            else
-            {
-                url = url.concat("ord= " + document.getElementById("Sorting").value);
-            }
-
-
-            console.log(url);
-
-
-            if (window.XMLHttpRequest) 
-            {
-                // code for IE7+, Firefox, Chrome, Opera, Safari
-                xmlhttp = new XMLHttpRequest();
-            } 
-            else 
-            {
-                // code for IE6, IE5
-                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-            }
-            xmlhttp.onreadystatechange = function() 
-            {
-                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) 
-                {
-                    document.getElementById("Producten").innerHTML  = xmlhttp.responseText;
-                }
-
-            }
-            console.log(url);
-            xmlhttp.open("GET",url,true);
-            xmlhttp.send();
+//
+//
+//            if(!categoriesSelected)
+//            {
+//                console.log("test");
+//                hideFooter();
+//            }
+//            else
+//            {
+//                console.log("meh");
+//            }
+//
+//
+//            if(url.slice(-1) == "&")
+//            {
+//                url = url.concat("& " + "ord= " + document.getElementById("Sorting").value);
+//            }
+//            else
+//            {
+//                url = url.concat("ord= " + document.getElementById("Sorting").value);
+//            }
+//
+//
+//            console.log(url);
+//
+//
+//            if (window.XMLHttpRequest) 
+//            {
+//                // code for IE7+, Firefox, Chrome, Opera, Safari
+//                xmlhttp = new XMLHttpRequest();
+//            } 
+//            else 
+//            {
+//                // code for IE6, IE5
+//                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+//            }
+//            xmlhttp.onreadystatechange = function() 
+//            {
+//                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) 
+//                {
+//                    document.getElementById("Producten").innerHTML  = xmlhttp.responseText;
+//                }
+//
+//            }
+//            console.log(url);
+//            xmlhttp.open("GET",url,true);
+//            xmlhttp.send();
         }
 
         function setCategorieSorting()
