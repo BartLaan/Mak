6a1739dfeb298ba02417e553c30be758ecaf0cfe
rien333 <rien334@gmail.com>
2015-01-27 12:16:02 +0100

    ProductCatalogus sorteer fix

--- productCatalogus.php
+++ productCatalogus.php
@@ -235,7 +235,7 @@ hr
         function urlCategorieen() 
         {
 //            var urlCategorie = <?php echo json_encode($_GET["categorie"]); ?>;
-            console.log(<?php echo json_encode($_GET["categorie"]); ?>);
+//            console.log(<?php echo json_encode($_GET["categorie"]); ?>);
             var categorienLijst = <?php echo json_encode($categorienArray); ?>;
             if (urlCategorie != "" && urlCategorie != null) 
             {
