6b84d10d68f6ff0998c6c606d12918b3f4c50c5f
rien333 <rien334@gmail.com>
2015-01-27 11:01:14 +0100

    Uitstekende rand bovenaan fix

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         overflow:hidden;
         position: absolute;
         top:14%; 
-        margin-top:10%;
+        margin-top:3%;
         border-color:red;
         min-height:300px;
         height:485px;
