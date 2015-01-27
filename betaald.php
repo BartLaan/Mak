f39df5fc8c0e706b902976e57064debd566a285d
Caiit <caitlin.lagrand@live.nl>
2015-01-27 11:45:10 +0100

    bestellen

--- betaald.php
+++ betaald.php
@@ -7,6 +7,7 @@
         </title>
         <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
         <link href="opmaakmenu.css" rel="stylesheet" type="text/css" />
+        <link href="opmaak.css" rel="stylesheet" type="text/css" />
 
     <style>
     h1.about, p.about
@@ -46,7 +47,7 @@
         ?>
         <div id="page">
             <div id="text">
-                <p> U hebt betaald! Bedankt voor uw bestelling! </p>
+                <p class="center"> U hebt betaald! Bedankt voor uw bestelling! </p>
                 <img src="images/barry_banner.jpg" alt="Barry's Bakery Banner" style="width:1000px">
                 <a href="https://ki30.webdb.fnwi.uva.nl/Mak/productCatalogus.php">
                     <img src="images/verderwinkelen.png" border="0" alt="Winkel verder" style="width:200px">
