ef83957efcd8e4ec7bd759671d59c18fdf241c08
simon <simon_boompie@hotmail.com>
2015-01-27 12:33:34 +0100

    betaald.php

--- betaald.php
+++ betaald.php
@@ -20,9 +20,9 @@
         <div id="page">
             <div id="text">
                 <p class="center"> U hebt betaald! Bedankt voor uw bestelling! </p>
-                <div class="betaald"> <img src="images/barry_banner.jpg" alt="Barry's Bakery Banner" style="width: 600px"> </div>
+                <div class="betaald"> <img src="images/barry_banner.jpg" alt="Barry's Bakery Banner" style="width: 650px"> </div>
                 <p class="center"> <a href="https://ki30.webdb.fnwi.uva.nl/Mak/productCatalogus.php">
-                    <img src="images/verderwinkelen.png" border="0" alt="Winkel verder" style="width:200px">
+                    <img src="images/verderwinkelen.png" onmouseover="this.src=\'images/verderwinkelenhover.png\'" onmouseout="this.src=\'images/verderwinkelen.png\'" alt="verderwinkelen" height="40">
                 </a> </p>
             </div>
         </div>
@@ -30,4 +30,4 @@
 <?php include 'footer.php'; ?>
     </body>
 
-<html>
+</html>
