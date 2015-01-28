a1e61e30439ecea7dd7c0a9bf4b34e040aaf1ed6
BartLaan <bartlaan96@gmail.com>
2015-01-28 13:37:41 +0100

    administratorpagina

--- Administratorpagina.php
+++ Administratorpagina.php
@@ -6,7 +6,11 @@
 	<link href="opmaakmenu.css" rel="stylesheet" type="text/css" />
     
 <style>
-    #knoppen li {
+    #knoppen {
+        margin-left: auto;
+        margin-right: auto;
+    }
+    #knoppen-lijst li {
         display: inline;
         list-style-type: none;
         padding-right: 20px;
@@ -18,11 +22,13 @@
     <div id="page">
     <div id="text">
         <h1>Welkom, administrator.</h1>
-        <ul id="knoppen">
-            <li><a href="productenbeheren.php"><img src="images/producten.png" onmouseover="this.src=\'images/productenhover.png\'" onmouseout="this.src=\'images/producten.png\'" alt="Producten Beheren" height="40"></a></li>
-            <li><a href="klantenbeheren.php"><img src="images/klanten.png" onmouseover="this.src=\'images/klantenhover.png\'" onmouseout="this.src=\'images/klanten.png\'" alt="Klanten Beheren" height="40"></a></li>
-            <li><a href="facturen.php"><img src="images/facturen.png" onmouseover="this.src=\'images/facturenhover.png\'" onmouseout="this.src=\'images/facturen.png\'" alt="Facturen Beheren" height="40"></a></li>
-        </ul>
+        <div id="knoppen">
+            <ul id="knoppen-lijst">
+                <li><a href="productenbeheren.php"><img src="images/producten.png" onmouseover="this.src=\'images/productenhover.png\'" onmouseout="this.src=\'images/producten.png\'" alt="Producten Beheren" height="40"></a></li>
+                <li><a href="klantenbeheren.php"><img src="images/klanten.png" onmouseover="this.src=\'images/klantenhover.png\'" onmouseout="this.src=\'images/klanten.png\'" alt="Klanten Beheren" height="40"></a></li>
+                <li><a href="facturen.php"><img src="images/facturen.png" onmouseover="this.src=\'images/facturenhover.png\'" onmouseout="this.src=\'images/facturen.png\'" alt="Facturen Beheren" height="40"></a></li>
+            </ul>
+        </div>
     </div>
     </div>
 <?php include 'footer.php'; ?>
