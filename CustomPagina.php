710e7578df270b0ef14b120ecf69fde3ad18567a
dabarryboy <barry1996@hotmail.com>
2015-01-28 20:04:49 +0100

    Added Cyan

--- CustomPagina.php
+++ CustomPagina.php
@@ -221,7 +221,7 @@ if (!empty($_POST['button'])) {
 			</div>
 			<form method = "post"; action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
 			<div class='ingredientChecker'>
-				<h4> Kies Uw Toppings: <span class = "vereist"> * <?php echo . $TOPPINGERR . ?> </span> </h4>
+				<h4> Kies Uw Toppings: <span class = "vereist"> * <?php echo . $TOPPINGERR . ;?> </span> </h4>
 				<p>
 					<?php include 'database_connect.php';
 						include 'TrimLeadingZeroes.php';
@@ -233,7 +233,7 @@ if (!empty($_POST['button'])) {
 							}
 					?>
 				</p>
-				<h4> Kies een vulling: <span class = "vereist"> * <?php echo . $VULLINGERR . ?> </span></h4>
+				<h4> Kies een vulling: <span class = "vereist"> * <?php echo . $VULLINGERR . ;?> </span></h4>
 				<p>
 					<?php
 						$VullingSQL = 'SELECT * FROM Ingredients WHERE Categorie = "vulling"';
@@ -246,7 +246,7 @@ if (!empty($_POST['button'])) {
 				</p>
 			</div>
 			<div class = 'ingredientChecker2'>
-				<h4> Kies een bodem: <span class = "vereist"> * <?php echo . $BODEMERR . ?> </span></h4>
+				<h4> Kies een bodem: <span class = "vereist"> * <?php echo . $BODEMERR . ;?> </span></h4>
 				<p>
 					<?php
 						$BodemSQL = 'SELECT * FROM Ingredients WHERE Categorie = "bodem"';
