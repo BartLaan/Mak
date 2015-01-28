addfe7b89f267505ae72bba66c7f79c3d6e048e6
BartLaan <bartlaan96@gmail.com>
2015-01-28 13:30:29 +0100

    administratorpagina

--- Administratorpagina.php
+++ Administratorpagina.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Barry's Bakery - Beheerder</title>
-	<link rel="stylesheet" type="text/css" >
+    <link href="opmaak.css" rel="stylesheet" type="text/css" />
 	<link href="opmaakmenu.css" rel="stylesheet" type="text/css" />
     
 <style>
@@ -18,12 +18,12 @@
     <div id="page">
     <div id="text">
         <h1>Welkom, administrator.</h1>
-    </div>
         <ul id="knoppen">
             <li><a href="productenbeheren.php"><img src="images/producten.png" onmouseover="this.src=\'images/productenhover.png\'" onmouseout="this.src=\'images/producten.png\'" alt="Producten Beheren" height="40"></a></li>
             <li><a href="klantenbeheren.php"><img src="images/klanten.png" onmouseover="this.src=\'images/klantenhover.png\'" onmouseout="this.src=\'images/klanten.png\'" alt="Klanten Beheren" height="40"></a></li>
             <li><a href="facturen.php"><img src="images/facturen.png" onmouseover="this.src=\'images/facturenhover.png\'" onmouseout="this.src=\'images/facturen.png\'" alt="Facturen Beheren" height="40"></a></li>
         </ul>
+    </div>
     </div>
 <?php include 'footer.php'; ?>
 </body>
