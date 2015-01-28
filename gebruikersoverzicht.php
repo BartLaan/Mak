278070c6fc7dc58bcc7cd9e2b460231ed552eaee
BartLaan <bartlaan96@gmail.com>
2015-01-28 18:42:55 +0100

    nog een ww veranderne

--- gebruikersoverzicht.php
+++ gebruikersoverzicht.php
@@ -436,11 +436,11 @@
     {
         if(document.getElementById("wachtwoord").value.length > 1 && document.getElementById("herWachtwoord").value.length > 1 )
         {
-            document.getElementById("submitButton").disabled = true;
+            document.getElementById("submitButton").disabled = false;
         }
         else
         {
-            document.getElementById("submitButton").disabled = false;
+            document.getElementById("submitButton").disabled = true;
         }
     }
 
