c306eac7311d450f55fd1490c0f38c4c4527137f
Caiit <caitlin.lagrand@live.nl>
2015-01-29 13:08:44 +0100

    productpagina

--- ProductPagina.php
+++ ProductPagina.php
@@ -176,36 +176,34 @@ function normalImg(x) {
                 }
             }
 
-            $productenSql = 'SELECT * FROM Product WHERE Product_ID=?';
-            $stmt = $db->prepare($productenSql);
-            $stmt->bindValue(1, $Product_Nr, PDO::PARAM_INT); 
-            $stmt->execute();
+            $productenSql = 'SELECT * FROM Product WHERE Product_ID="'.$Product_Nr.'"';
+            $product_ophalen = $db->prepare($productenSql); 
+            $product_ophalen->execute();
 
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $result = $product_ophalen->fetchAll(PDO::FETCH_ASSOC);
 
             if (!$result) {
                 echo "<br> <p class='center'> Deze pagina bestaat niet. Klik <a href='productCatalogus.php'>hier</a> om terug te gaan naar het overzicht.</p>";
             } else {
 
-                foreach ($result as $row){
+                # foreach ($result as $row){
 
                     echo "<div class='productVak'>
-                            <h1>".$row['Productnaam']."</h1>";
-                        /*echo '<img src="images/' . $row["img_filepath"] . '" alt="' . $row["Productnaam"] . '">';*/
-                        echo '<img onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0" src="images/' . $row["img_filepath"] . '" alt="' . $row["Productnaam"] . '" width="200">';
+                            <h1>".$result['Productnaam']."</h1>";
+                        echo '<img onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0" src="images/' . $result["img_filepath"] . '" alt="' . $row["Productnaam"] . '" width="200">';
 
    
                         $check_aanbieding = false;
                         $prijs = trimLeadingZeroes($row["Prijs"]);
 
-                        if ($row['Aanbieding'] != 00000000.00) {
-                            $aanbieding =  trimLeadingZeroes($row['Aanbieding']); 
+                        if ($result['Aanbieding'] != 00000000.00) {
+                            $aanbieding =  trimLeadingZeroes($result['Aanbieding']); 
                             $check_aanbieding = true;
                         }
 
                         echo "<div class='beschrijvingsVak'>
                             <h3>Beschrijving </h3>
-                            <p>".$row['Beschrijving']."</p>";
+                            <p>".$result['Beschrijving']."</p>";
                             if ($check_aanbieding == true) {
                                 echo "<p> Prijs: <em> &#128;".$prijs."</em></p>";
                                 echo "<p class='afgeprijst'>  &#128;".$aanbieding." </p>";
@@ -225,8 +223,8 @@ function normalImg(x) {
             
                         <div class='tekstVak'>
                             <h3>Specificaties</h3>
-                            <p> Gewicht: <b>".$row['Gewicht']."</b> gram</p>
-                            <p> Extra informatie: <b>".$row['SecundaireInfo']."</b></p>
+                            <p> Gewicht: <b>".$result['Gewicht']."</b> gram</p>
+                            <p> Extra informatie: <b>".$result['SecundaireInfo']."</b></p>
                         </div>
                         <hr>";
 
