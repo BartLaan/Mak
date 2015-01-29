b4fa683966e6bae5a09681e8b5881a430b0d5528
BartLaan <bartlaan96@gmail.com>
2015-01-29 16:00:06 +0100

    klantenBeheren

--- klantenBeheren.php
+++ klantenBeheren.php
@@ -11,30 +11,46 @@
 
 <div id='page'>
 <div id='text'>
-<h1>Klanten</h1>
-
-<table>
-	<tr>
-		<th>Klant_ID</th>
-		<th>Naam</th>
-		<th>Emailadres</th>
-	</tr>
 <?php
-	$stmt = $db->prepare("SELECT Klant_ID, Achternaam, Voornaam, Tussenvoegsel, Emailadres FROM Klant ORDER BY Emailadres");
-	$stmt->execute();
-	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if (isset($_SESSION['Klant_ID'])) {
+        $query = "SELECT Emailadres FROM Klant WHERE Klant_ID='" . $_SESSION['Klant_ID'] . "'AND Administrator=1";
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $result = $stmt->fetch(); 
+    } else {
+        echo 'U bent niet gemachtigd om deze pagina te bekijken. Log in als administrator om verder te gaan.';
+        echo '<a href="/log_in.php">Inloggen</a>';
+    }
+    if ($result && strlen($result["Emailadres"]) > "0") {
+    	echo '
+			<h1>Klanten</h1>
 
-	foreach ($result as $klant) {
-		$klantLink = "<a href='klantGegevens.php?id=". $klant['Klant_ID'] .">";
-		echo '<tr>
-			<td>'. $klantLink . $klant['Klant_ID'] .'</a></td>
-			<td>'. $klantLink . $klant['Achternaam'] .', '. $klant['Voornaam'] .' '. $klant['Tussenvoegsel'] .'</a></td>
-			<td>'. $klantLink . $klant['Emailadres'] .'</a></td>
-		</tr>
+			<table>
+				<tr>
+					<th>Klantnummer</th>
+					<th>Naam</th>
+					<th>Emailadres</th>
+				</tr>
 		';
+		$stmt = $db->prepare("SELECT Klant_ID, Achternaam, Voornaam, Tussenvoegsel, Emailadres FROM Klant");
+		$stmt->execute();
+		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+		foreach ($result as $klant) {
+			$klantLink = "<a href='klantGegevens.php?id=". $klant['Klant_ID'] .">";
+			echo '<tr>
+				<td>'. $klantLink . $klant['Klant_ID'] .'</a></td>
+				<td>'. $klantLink . $klant['Achternaam'] .', '. $klant['Voornaam'] .' '. $klant['Tussenvoegsel'] .'</a></td>
+				<td>'. $klantLink . $klant['Emailadres'] .'</a></td>
+			</tr>
+			';
+		}
+		echo "</table>";
+	} else {
+		echo "U bent niet gemachtigd om deze pagina te bekijken.";
 	}
 ?>
-</table>
+			
 </div>
 </div>
 </body>
