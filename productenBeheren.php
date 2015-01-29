b8d8fc07bda73d2626e294ab444101b2760aab6a
rien333 <rien334@gmail.com>
2015-01-29 13:21:49 +0100

    Producten toevoegen

--- productenBeheren.php
+++ productenBeheren.php
@@ -400,7 +400,7 @@
 //                    
                     var reasons =  xmlhttp.responseText.match(/\[(.*?)\]/);
                     console.log(errorMessage);
-                    displayError(caller, reasons[1], errorMessage[1].slice(0,-2));
+                    displayError(caller, reasons[1], (errorMessage[1].slice(0,-2)).split("[")[0]);
                     revertBackOldValue(caller);
                 }
             }
