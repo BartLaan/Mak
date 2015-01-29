b08ab99cbd493e4fe9ec62a27e544612020dfc23
rien333 <rien334@gmail.com>
2015-01-29 02:51:59 +0100

    Producten toevoegen

--- productenBeheren.php
+++ productenBeheren.php
@@ -351,6 +351,7 @@
     {
         var url = "ValidateProductInput.php?";
         var row = getRow(caller);
+        console.log(row.cells);
         for(var i = 0; i < row.cells.length; i++)
         {
             if( row.cells[i].tagName == "INPUT")
