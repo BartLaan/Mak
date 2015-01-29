123e6ae8d7635a49590769a27ed2ce884ca7be4a
Caiit <caitlin.lagrand@live.nl>
2015-01-29 13:09:30 +0100

    productpagina

--- ProductPagina.php
+++ ProductPagina.php
@@ -228,7 +228,7 @@ function normalImg(x) {
                         </div>
                         <hr>";
 
-                }
+                #}
             
                         echo "<div class='tekstVak'>
 
