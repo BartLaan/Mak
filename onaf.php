9ac1fba4d4bc4f7aec5fec39201022dfef0fd54e
dabarryboy <barry1996@hotmail.com>
2015-01-30 12:34:45 +0100

    Doorverwijzingspagina0

--- onaf.php
+++ onaf.php
@@ -8,7 +8,7 @@
 	<?php include 'menu.php';?>
 	<div id = "page">
 	<div id = "text">
-	<h1> Barry is nog bezig met het zoeken naar de juiste ingrediënten. </h1>
+	<h1> Barry is nog bezig met het zoeken naar de juiste ingrediënten. Onze excuses voor het ongemak. </h1>
 	</div>
 	</div>
 	<?php include "footer.php";?>
