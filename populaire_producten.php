0baf7da4bc32f9193feca2665e9769d330076717
Caiit <caitlin.lagrand@live.nl>
2015-01-30 02:18:40 +0100

    index

--- populaire_producten.php
+++ populaire_producten.php
@@ -24,8 +24,7 @@ foreach($pop_producten as $x => $x_value) {
 	$pop_pr->execute();
 
 	foreach ($pop_pr as $product) { 
-	echo '<a href="ProductPagina.php?id=' . $x . '">
-	<div class="product">
+	echo '<a href="ProductPagina.php?id='.$x.'"><div class="product">
         <div class="productAfbeelding">
             <img src="images/' .$product["img_filepath"].'" alt="'.$product["img_filepath"].'"> </img>
 
@@ -33,8 +32,7 @@ foreach($pop_producten as $x => $x_value) {
         <div class="productBeschrijving">
             <p> '.$product["Productnaam"].' <br> '.trimLeadingZeroes($product["Prijs"]).'</p>
         </div>
-    </div>
-    </a>';
+    </div>    </a>';
 	}
 	if ($count == 5) break;
 } 
