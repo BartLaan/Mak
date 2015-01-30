61255b51aa0d16eb1d38cd117ae2a81f3a92f744
rien333 <rien334@gmail.com>
2015-01-30 03:31:36 +0100

    Producten toevoegen

--- productenBeheren.php
+++ productenBeheren.php
@@ -642,7 +642,7 @@
         document.getElementById("minusButton").style.visibility = "visible";
         var row = getRow(caller);
         var rowOffset = row.rowIndex;
-        document.getElementById("minusButton").style.top = 17.5 * (rowOffset) + 295 + "px" ;
+        document.getElementById("minusButton").style.top = 18 * (rowOffset) + 295 + "px" ;
     }
 
     function deleteCurrentRow()
