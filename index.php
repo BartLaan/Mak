300e35145c698e9c18208dab1377d3d20d37caf6
rien333 <rien334@gmail.com>
2015-01-30 13:47:26 +0100

    Verwijderen prullenbak en alert geadd

--- index.php
+++ index.php
@@ -511,7 +511,7 @@
             }
             image2.style.opacity = op2;
             image2.style.filter = 'alpha(opacity=' + op2 * 100 + ")";
-            op2 += 0.17; 
+            op2 += 0.1; 
 
 
         }, 0.1);
