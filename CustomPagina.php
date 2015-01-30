4e7707db259f9e1e5e77f25bfd34fc600f14b0e9
dabarryboy <barry1996@hotmail.com>
2015-01-30 03:55:51 +0100

    AJAX

--- CustomPagina.php
+++ CustomPagina.php
@@ -171,7 +171,6 @@ function visualizeTopping(topping, waarde){
 	if(xmlhttp == null){
 		alert("Gebruik alstublieft een betere browser.");
 	}
-	write(topping, waarde);
 	else if(waarde != TRUE){
 		if(topping == kaars){
 			document.getElementByID("topping1").innerHTML = "";
@@ -276,7 +275,7 @@ if (!empty($_POST['button'])) {
 					<span class = "preview" id = "bodem"> <img src ="images/projectBarry/bodem1.png" alt = "preview" style = "width:80%; height:30%"> </span>
 					<span class = "preview" id = "vulling"> <img src ="images/projectBarry/vulling1.png" alt = "preview" style = "width:80%; height:20%"> </span>
 					<span class = "preview" id = "glazuur"> <img src ="images/projectBarry/glazuur1.png" alt = "preview" style = "width:80%; height:15%"> </span>
-					<span class = "preview" id = "topping1"> <img src ="images/projectBarry/topping1.png" alt = "preview" style = "width:80%;"> </span>
+					<span class = "preview" id = "topping1"> </span>
 					<span class = "preview" id = "topping2"> </span>
 					<span class = "preview" id = "topping3"> </span>
 					<span class = "preview" id = "topping4"> </span>
