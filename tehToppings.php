a9499330985a9c2fd54014741580f60012c52d38
dabarryboy <barry1996@hotmail.com>
2015-01-31 17:57:45 +0100

    Verder Testen

--- tehToppings.php
+++ tehToppings.php
@@ -6,7 +6,8 @@
 <?php
 	include 'database_connect.php';
 	$q = strval($_GET('q'));
-	$sql = $db -> prepare("SELECT Foto FROM Ingredients WHERE Naam = '"$q"' AND Categorie = 'topping'");
+	die($q);
+	$sql = $db -> prepare('SELECT Foto FROM Ingredients WHERE Naam = "'$q'" AND Categorie = "topping"');
 	$sql -> execute();
 	while($row = $foto -> fetch()){
 		echo("<img src ='images/".$foto."' alt = 'preview' style = 'width:80%;'>");
