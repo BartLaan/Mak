ee7850d0a5160b9f6ca43ac9b7efb9cf5d59ddcd
dabarryboy <barry1996@hotmail.com>
2015-01-31 17:27:07 +0100

    Verder Testen

--- CustomPagina.php
+++ CustomPagina.php
@@ -161,7 +161,7 @@ p.afgeprijst
 <script type = "text/javascript">
 
 function visualizeTopping(topping, waarde){
-	console.log(topping);
+	console.log(waarde);
 	var xmlhttp;
 	if(window.XMLHttpRequest){
 		xmlhttp = new XMLHttpRequest();
