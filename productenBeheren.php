cccda21efd2ce492be43d6c35a342f40c1078555
BartLaan <bartlaan96@gmail.com>
2015-02-01 18:43:48 +0100

    Revert "css productbeherne"
    This reverts commit ff0ddc0913a8478659d46f61dc16fb879d182de2.

--- productenBeheren.php
+++ productenBeheren.php
@@ -2,16 +2,17 @@
 <html>
 <head>
     <link href="opmaakmenu.css" rel="stylesheet" type="text/css" />
-    <title> Product Beheer</title>
+	<title> Productbeheer</title>
 
-    <style>
+	<style>
 
     table
     {
+        margin-left:5%;
         max-width:80%;
         width:80%;
         float:left;
-        border-collapse:collapse; 
+		border-collapse:collapse; 
     }
 
     tr
@@ -19,18 +20,19 @@
         clear:both;
     }
 
-    table, th, td {
-        border: 1px solid #E3E3E3;
-    }
+	table {
+		border: 1px solid #854442;
+	}
 
-    th,td 
+	th,td 
     {
+        border: 1px solid #E3E3E3;
         width:8%;
         max-width:15%;
-    }
+	}
 
     /* Remove textfield styling  */
-    input[type = "text"] 
+	input[type = "text"] 
     {
         background-color: transparent;
         border: none; 
@@ -86,10 +88,27 @@
         position:relative;
     }
 
+    h1
+    {
+        font-family: 'Helvetica Light', 'Helvetica', Arial, sans-serif;
+        text-align: center;
+    }
+    h2
+    {
+        font-family: 'Helvetica Light', 'Helvetica', Arial, sans-serif;
+        text-align: center;
+    }
 
     h4
     {
+        font-family: 'Helvetica Light', 'Helvetica', Arial, sans-serif;
+        text-align: center;
         font-weight:normal;
+    }
+    p
+    {
+        font-family: 'Helvetica Light', 'Helvetica', Arial, sans-serif;
+        text-align: center;
     }
 
     
@@ -154,6 +173,7 @@
         width:100%;
         font-size:80%;
         text-align:left;
+        font-family: arial;
         font-weight:normal;
         font-style:none;
         text-overflow: ellipsis;
@@ -168,11 +188,11 @@
     {
         width:100%;
         margin-top:3.7%;
-        border:none; <!-- looks better on all browsers except firefox -->
+        border:none; /* looks better on all browsers except firefox */
     }
 
  
-    /* Some sub information about the information in the header */
+    /* Some sub information about the information in the header *?
     .extraHeaderInfo
     {
         margin-top:1%;
@@ -189,7 +209,7 @@
         max-width:50%;
         max-heigth:70%;
     }
-    </style>
+	</style>
 
 </head>
 <body>
@@ -198,12 +218,10 @@
     include 'menu.php'; 
     include 'TrimLeadingZeroes.php';
 ?>
-    <div id="text" style="padding-left:5%">
-    <h1> Product Beheer </h1>  
-    <h2> Producten </h2>
+    <div id="text">
+	<h1> Productbeheer </h1>  
 
-
-    <table id="productenTable">
+	<table id="productenTable">
         <?php
             $f = fopen("/tmp/phpLog.txt", "w");
 
@@ -292,7 +310,7 @@
                     }
                     else
                     {
-                        echo '<td> <input onblur="validateInput(this)" onfocus="processInput(this)" type="text" class="' . $key . '" value="' . $value . '"> </td>';
+          		        echo '<td> <input onblur="validateInput(this)" onfocus="processInput(this)" type="text" class="' . $key . '" value="' . $value . '"> </td>';
                     }
                 }
                 echo '</tr>';
@@ -300,25 +318,20 @@
             fclose($f); 
         ?>
 
-    </table> 
+	</table> 
     
-    <div class = "tabelHulpmiddelen">
+    <div class = "tabelHulpmiddelen" align="center">
 
         <div class = "knopRij">
-        <div class = "roundButton" onclick="addRow()" style="margin-top:10px;"> +  </div> <p style="display:inline-block"> Voeg een product toe...  </p> 
+		<div class = "roundButton" onclick="addRow()" style="margin-top:10px;"> +  </div> <p style="display:inline-block"> Voeg een product toe...  </p> 
         </div>
 
-    
-        <h4>
-            Uitgebreide Omschrijving <br>
-            <textarea  id="omschrijving" onchange="updateOmschrijving()"  rows = "20" cols = "50" value=""> </textarea>
-        </h4>
-    
-        <form action="">
-        <h2> Voeg Een Batch Toe </h2>
-        <input type="file" name="" accept="image/*">
-        <button type="button">Submit Batch</button>
-        </form>
+        <div class="uitgebrOmschrijving" align="center">
+    	<h4>
+    		Uitgebreide Omschrijving <br>
+    		<textarea  id="omschrijving" onchange="updateOmschrijving()" rows = "20" cols = "50" value=""> </textarea>
+    	</h4>
+        </div>
     </div>
     </div>
 
