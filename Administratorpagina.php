019ecade6d8ab961bc3fab5c639449bb0739b76f
BartLaan <bartlaan96@gmail.com>
2015-02-02 00:14:42 +0100

    link change

--- Administratorpagina.php
+++ Administratorpagina.php
@@ -41,7 +41,7 @@
             <br /><br />
             <div id="knoppen">
                 <ul id="knoppen-lijst">
-                    <li><a href="productenBeheren.php"><img src="images/producten.png" onmouseover="this.src=\'images/productenhover.png\'" onmouseout="this.src=\'images/producten.png\'" alt="Producten Beheren" height="50"></a></li>
+                    <li><a href="productenoverzicht.php"><img src="images/producten.png" onmouseover="this.src=\'images/productenhover.png\'" onmouseout="this.src=\'images/producten.png\'" alt="Producten Beheren" height="50"></a></li>
                     <li><a href="klantenBeheren.php"><img src="images/klanten.png" onmouseover="this.src=\'images/klantenhover.png\'" onmouseout="this.src=\'images/klanten.png\'" alt="Klanten Beheren" height="50"></a></li>
                     <li><a href="facturen.php"><img src="images/facturen.png" onmouseover="this.src=\'images/facturenhover.png\'" onmouseout="this.src=\'images/facturen.png\'" alt="Facturen Beheren" height="50"></a></li>
                 </ul>
