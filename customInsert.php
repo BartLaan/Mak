9d673ca426e97cefadadf20fac34a7d9061ce912
BartLaan <bartlaan96@gmail.com>
2015-02-02 00:48:32 +0100

    barry upgrade

--- customInsert.php
+++ customInsert.php
@@ -15,38 +15,40 @@
 		$sql -> execute();
 		$res = $sql->fetchAll(PDO::FETCH_ASSOC);
 		if(!isset($res['ID'])){
-			$stmt = $db -> prepare('INSERT INTO customingredienten(bodem, vulling, glazuur, topping1, topping2, topping3) VALUES(?,?,?,?,?,?)');
-			$stmt -> bindValue(1, $vulling, PDO::PARAM_STR);
-			$stmt -> bindValue(2, $bodem, PDO::PARAM_STR);
-			$stmt -> bindValue(3, $glazuur, PDO::PARAM_STR);
-			$stmt -> bindValue(4, $topping1, PDO::PARAM_INT);
-			$stmt -> bindValue(5, $topping2, PDO::PARAM_INT);
-			$stmt -> bindValue(6, $topping3, PDO::PARAM_INT);
+			$stmt = $db -> prepare('INSERT INTO customingredienten(vulling, bodem, glazuur, topping1, topping2, topping3) VALUES("'.$vulling.'","'.$bodem.'","'.$glazuur.'",'.$topping1.','.$topping2.','.$topping3.')');
 			$stmt -> execute();
-			$asdf = $db -> prepare('SELECT ID FROM customingredienten WHERE bodem = "'.$bodem.'" AND vulling = "'.$vulling.'" AND glazuur = "'.$glazuur.'" AND topping1 = "'.$topping1.'" AND topping2 = "'.$topping2.'" AND topping3 = "'.$topping3.'"');
-			$asdf -> execute();
-			while($reference = $asdf -> fetch()){
-				$taart = $db -> prepare('INSERT INTO Product(Productnaam, Categorie, Prijs, Voorraad, img_filepath, Aanbieding, customIngredientenID) VALUES(?,?,?,?,?,?,?)');
-				$taart -> bindValue(1, "Custom", PDO::PARAM_STR);
-				$taart -> bindValue(2, "Aangepast", PDO::PARAM_STR);
-				$taart -> bindValue(3, 59.99, PDO::PARAM_INT);
-				$taart -> bindValue(4, -1, PDO::PARAM_INT);
-				$taart -> bindValue(5, "images/AMERICA.jpg", PDO::PARAM_STR);
-				$taart -> bindValue(6, 0.0, PDO::PARAM_INT);
-				$taart -> bindValue(7, $reference, PDO::PARAM_STR);
-				$taart -> execute();
-				$gthf = $db -> prepare('SELECT Product_ID FROM Product WHERE customIngredientenID = "'.$reference.'"');
-				$gthf -> execute();
-				while($taartID = $gthf -> fetch()){
-					echo($taartID);
-				}
-			}
-		}
-		else{
-			$stmt = $db -> prepare('SELECT Product_ID FROM Product WHERE customIngredientenID = "'.$reference.'"');
+			
+			$newID = $db -> prepare('SELECT ID FROM customingredienten WHERE bodem = "'.$bodem.'" AND vulling = "'.$vulling.'" AND glazuur = "'.$glazuur.'" AND topping1 = "'.$topping1.'" AND topping2 = "'.$topping2.'" AND topping3 = "'.$topping3.'"');
+			$newID -> execute();
+			$result = $newID->fetchAll();
+			$freshID = $result['ID'];
+
+			$inProducten = $db-> prepare('INSERT INTO Product(Productnaam, Categorie, Prijs, Voorraad, img_filepath, Aanbieding, customIngredientenID) VALUES("Custom taart","Aangepast",59.99,1,"images/AMERICA.jpg",0.0,'.$freshID).')');
+			$inProducten-> execute();
+
+			// while($reference = $newID -> fetch()){
+			// 	$taart = $db -> prepare('INSERT INTO Product(Productnaam, Categorie, Prijs, Voorraad, img_filepath, Aanbieding, customIngredientenID) VALUES(?,?,?,?,?,?,?)');
+			// 	// $taart -> bindValue(1, "Custom", PDO::PARAM_STR);
+			// 	// $taart -> bindValue(2, "Aangepast", PDO::PARAM_STR);
+			// 	// $taart -> bindValue(3, 59.99, PDO::PARAM_INT);
+			// 	// $taart -> bindValue(4, -1, PDO::PARAM_INT);
+			// 	// $taart -> bindValue(5, "images/AMERICA.jpg", PDO::PARAM_STR);
+			// 	// $taart -> bindValue(6, 0.0, PDO::PARAM_INT);
+			// 	// $taart -> bindValue(7, $reference, PDO::PARAM_STR);
+			// 	$taart -> execute();
+			$gthf = $db -> prepare('SELECT Product_ID FROM Product WHERE customIngredientenID = "'.$freshID.'"');
+			$gthf -> execute();
+			$result = $gthf->fetchAll();
+			echo $result['Product_ID'];
+				// while($taartID = $gthf -> fetch()){
+				// 	echo($taartID);
+				// }
+		
+		} else{
+			$stmt = $db -> prepare('SELECT Product_ID FROM Product WHERE customIngredientenID = "'.$res['ID'].'"');
 			$stmt -> execute();
-			while($taartID = $stmt -> fetch()){
-				echo $taartID;
+			$result = $stmt->fetchAll();
+			echo $taartID;
 			}
 		}
 	?>
